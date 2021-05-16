cc832b069174eb91537e501be6293ff460aea4d3
giorgos apostolidis <apostolidesg1@gmail.com>
2021-05-16 20:51:58 +0300

    first commit

---
+++ vue-lottery-game/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(VueAxios, axios);
+
+app.mount("#app");
